d6307c3797159fe56b99eb41b7da5e2bd232bab7
Cat Gelinas <catgelinas78@gmail.com>
2016-08-26 19:06:57 -0400

    Add node modules and scss file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require( 'express' );
 var app = express();
 
 app.get( '/', function( req, res ) {
-	res.send("Hello Memory Game Peeps!");
+	res.send("Hello Peeps!");
 } );
 
 app.listen(3000, function() {
