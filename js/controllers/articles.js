c2d2b6042ba42e34c152d19b2a19985c959bf036
Cat Gelinas <catgelinas78@gmail.com>
2016-08-27 10:56:59 -0400

    Add initialize function for http request in ArticlesController

--- js/controllers/articles.js
+++ js/controllers/articles.js
@@ -5,27 +5,36 @@
       function( $scope, $timeout, $http ) {
 
 
-        //Put conditional that checks the dbCollection.count
-        // If count == 0, save into the db.
-        $http.get(' https://s3.amazonaws.com/mbgd/feed/prod-test-7fc12640-6f09-4461-b683-3e55acdfd4f4.json').
-          success(
-            function( data, status, headers, config ) {
-              $scope.contentCollection = data;
 
-              $scope.contentCollection.forEach(
-                function( c ) {
-                  c.body = String( c.body ).replace(/<[^>]+>/gm, '');
-                }
-              );
 
-              console.log("$scope.contentCollection: ", $scope.contentCollection );
-              // save each object into the db.
+
+
+
+        var initializeArticlesController = function() {
+          //Put conditional that checks the dbCollection.count
+          // If count == 0, save into the db.
+
+          $http.get(' https://s3.amazonaws.com/mbgd/feed/prod-test-7fc12640-6f09-4461-b683-3e55acdfd4f4.json').
+            success(
+              function( data, status, headers, config ) {
+                $scope.contentCollection = data;
+
+                $scope.contentCollection.forEach(
+                  function( c ) {
+                    c.body = String( c.body ).replace(/<[^>]+>/gm, '');
+                  }
+                );
+                // save each object into the db.
+              }
+            ).
+            error( function( data, status, headers, config ) {
+              // log error
             }
-          ).
-          error( function( data, status, headers, config ) {
-            // log error
-          } );
+          );
+          // end
+        }
 
+        initializeArticlesController();
 
 
 
