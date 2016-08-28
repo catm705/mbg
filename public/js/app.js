b3331fc8125b31d44dd22c057dfae4228a8ff29d
Cat Gelinas <catgelinas78@gmail.com>
2016-08-28 16:36:43 -0400

    Add ng-Route and ng-view

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,23 @@
 ( function() {
+  var myApp = angular.module( 'articles', [ 'ngRoute' ] );
 
-  angular
-    .module( 'articles', [] );
+  myApp.config(['$routeProvider','$locationProvider',
+    function( $routeProvider, $locationProvider ) {
+      $routeProvider
+      .when('/', {
+         templateUrl: '/views/list.html',
+         controller: 'ArticlesController'
+      })
+
+      .when('/edit', {
+         templateUrl: '/views/edit.html',
+         controller: 'EditController'
+      })
+      .otherwise({
+         redirectTo: '/'
+      } );
+    }
+  ]);
+
 
 } )();
---
+++ public/js/controllers/edit_controller.js
@@ -0,0 +1,19 @@
+(function(){
+
+  angular.module( 'articles')
+  .controller( "EditController",
+    function( $scope, $timeout, $http ) {
+
+
+
+      var initializeEditController = function() {
+        //Put conditional that checks the dbCollection.count
+        // If count == 0, save into the db.
+        $http.get('/articleslist');
+
+      }
+
+      initializeEditController();
+    }
+  );
+} )();
