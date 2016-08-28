b304d307934c409d4a43921c3d0c15170d6b12f8
Cat Gelinas <catgelinas78@gmail.com>
2016-08-28 16:47:35 -0400

    Add edit page

--- public/js/controllers/edit_controller.js
+++ public/js/controllers/edit_controller.js
@@ -10,7 +10,7 @@
         //Put conditional that checks the dbCollection.count
         // If count == 0, save into the db.
         $http.get('/articleslist');
-
+        console.log("EditController and views/edit.html")
       }
 
       initializeEditController();
